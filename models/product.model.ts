ab433dc44dd2af0de55b4c9a0ed94e5f852417e9
Mike <mikerockmusic51@gmail.com>
2025-03-30 15:55:39 +0100

    Add Product model with validation and schema definition

---
+++ models/product.model.ts
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A product must have a name"],
+    unique: true,
+    trim: true,
+    maxlength: [
+      40,
+      "A product name must have less or equal than 40 characters",
+    ],
+    minlength: [1, "A product name must have more or equal than 1 character"],
+  },
+
+  price: {
+    type: Number,
+    required: [true, "A product must have a price"],
+    min: [0, "A product price must be >= 0"],
+  },
+
+  description: {
+    type: String,
+    required: [true, "A product must have a description"],
+    trim: true,
+    maxlength: [500, "Product's description must be <= 500 characters"],
+
+  },
+
+  images: {},
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export const ProductModel = mongoose.model("Product", ProductSchema);
