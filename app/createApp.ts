7be489079fe9e6c21d35a8e8ec00c445f8ff6998
Mike <mikerockmusic51@gmail.com>
2025-04-01 11:12:33 +0100

    feat: enable parsing of URL-encoded data in express app

--- app/createApp.ts
+++ app/createApp.ts
@@ -9,6 +9,7 @@ dotenv.config();
 export function createApp() {
     const app = express();
     app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
     app.use(router);
     app.use(errorHandler);
 
